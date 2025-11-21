bffbc39164afd037481e927ae3cfd2a1d9e3359c
MilkeeyCat <milkeeycat@gmail.com>
2025-11-21 23:14:36 +0200

    feat(dgen): add `Address`, `Block` & `ConditionCode` operands

--- tools/dgen/dgen/targets/amd64/operands.py
+++ tools/dgen/dgen/targets/amd64/operands.py
@@ -3,6 +3,19 @@ from typing import Self
 from dgen.base.operands import HasRegisterClass, Operand, Predicate, Register
 from . import register_classes as rcs
 
+GPR8 = Register([HasRegisterClass(rcs.GPR8)])
+GPR16 = Register([HasRegisterClass(rcs.GPR16)])
+GPR32 = Register([HasRegisterClass(rcs.GPR32)])
+GPR64 = Register([HasRegisterClass(rcs.GPR64)])
+
+
+class Address(Operand):
+    def __init__(self):
+        super().__init__("AddressMode", [], "emit_address")
+
+
+ADDR = Address()
+
 
 class Memory(Operand):
     def __init__(self, predicates: list["Predicate[Self]"], print_method: str):
@@ -14,7 +27,10 @@ I16MEM = Memory([], "emit_word_memory")
 I32MEM = Memory([], "emit_dword_memory")
 I64MEM = Memory([], "emit_qword_memory")
 
-GPR8 = Register([HasRegisterClass(rcs.GPR8)])
-GPR16 = Register([HasRegisterClass(rcs.GPR16)])
-GPR32 = Register([HasRegisterClass(rcs.GPR32)])
-GPR64 = Register([HasRegisterClass(rcs.GPR64)])
+
+class ConditionCode(Operand):
+    def __init__(self):
+        super().__init__("ConditionCode", [], "emit_condition_code")
+
+
+CCODE = ConditionCode()
